c7559cda64dced1c6a58822ec8dde21c1e0886f0
mshingan <31327558+mshingan@users.noreply.github.com>
2018-02-25 17:18:50 -0800

    third commit

--- String.cpp
+++ String.cpp
@@ -1,14 +1,4 @@
-// String.cpp
-//
-// ICS 46 Winter 2018
-// Project #0: Getting to Know the ICS 46 VM
-//
-// Implement all of your String member functions in this file.
-//
-// Note that the entire standard library -- both the C Standard
-// Library and the C++ Standard Library -- is off-limits for this
-// task, as the goal is to exercise your low-level implementation
-// skills (pointers, memory management, and so on).
+
 #include "String.h"
 #include "OutOfBoundsException.h"
 #include <iostream>
