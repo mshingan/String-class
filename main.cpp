f95441b274fbc4851448b4d3816df21cb6221118
Manasi Shingane <manasishingane@Manasis-MacBook-Air.local>
2018-02-25 17:00:48 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StringClass
+//
+//  Created by Manasi Shingane on 2/25/18.
+//  Copyright © 2018 Manasi Shingane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
